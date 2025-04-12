474b56008f50495e2fa4d91150b4cbce1466b256
Shikiyo <shikiyo@archjk>
2025-04-12 18:25:17 -0300

    utils: add LenSpliter

--- build.zig
+++ build.zig
@@ -68,19 +68,26 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const lib_unit_tests = b.addTest(.{
+    const utils_unit_tests = b.addTest(.{
+        .root_module = utils_mod,
+    });
+
+    const run_utils_unit_tests = b.addRunArtifact(utils_unit_tests);
+
+    const sim_unit_tests = b.addTest(.{
         .root_module = sim_mod,
     });
 
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    const run_sim_unit_tests = b.addRunArtifact(sim_unit_tests);
 
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
-    });
+    // const exe_unit_tests = b.addTest(.{
+    //     .root_module = exe_mod,
+    // });
 
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    // const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&run_utils_unit_tests.step);
+    test_step.dependOn(&run_sim_unit_tests.step);
+    // test_step.dependOn(&run_exe_unit_tests.step);
 }
--- src/utils.zig
+++ src/utils.zig
@@ -98,3 +98,65 @@ pub fn Buffer(comptime T: type, comptime Idx: type, comptime capacity: Idx) type
         };
     };
 }
+
+pub fn LenSpliter(comptime Int: type) type {
+    std.debug.assert(@typeInfo(Int) == .int);
+    return struct {
+        len: Int,
+
+        pub fn iterator(self: @This(), splits: Int) Iterator {
+            return Iterator.init(splits, self.len);
+        }
+
+        pub const Iterator = struct {
+            base: Int,
+            rem: Int,
+            splits: Int,
+            i: Int = 0,
+
+            pub fn init(splits: Int, len: Int) Iterator {
+                std.debug.assert(splits <= len);
+                std.debug.assert(0 < splits);
+                return .{
+                    .base = len / splits,
+                    .rem = len % splits,
+                    .splits = splits,
+                };
+            }
+
+            pub fn next(it: *Iterator) ?Int {
+                const State = enum { start, mid, last };
+                const state: State = if (it.splits <= it.i)
+                    return null
+                else if (it.i < it.splits/2)
+                    .start
+                else if (it.i < it.splits - it.splits/2)
+                    .mid
+                else
+                    .last;
+                const off = @as(Int, switch (state) {
+                    .start => if (it.i < it.rem/2) 1 else 0,
+                    .mid => it.rem & 1,
+                    .last => if (it.splits - it.rem/2 <= it.i) 1 else 0,
+                });
+                it.*.i += 1;
+                return it.base + off;
+            }
+        };
+    };
+}
+
+test "LenSpliter preserves sum" {
+    const len = 64;
+    const spliter = LenSpliter(u8){ .len = len };
+    for (1..(len+1)) |splits| {
+        const avg = len / splits;
+        var sum = @as(usize, 0);
+        var it = spliter.iterator(@intCast(splits));
+        while (it.next()) |split| {
+            try std.testing.expect(split == avg or split == avg + 1);
+            sum += split;
+        }
+        try std.testing.expectEqual(len, sum);
+    }
+}
